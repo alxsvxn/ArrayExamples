b2db101ddfff88d3feedad774dd41ee78de42aa2
Alexis Villagran <alexisvillagran18@hotmail.com>
2025-02-10 08:27:18 -0700

    Added simple array example

---
+++ ArrayExamples/ArrayExamples.vb
@@ -0,0 +1,21 @@
+﻿'Header
+
+Option Strict On
+Option Explicit On
+Option Compare Text
+Module ArrayExamples
+
+    Sub Main()
+        'Arrays are cool
+
+        Dim fruits(5) As String
+
+        fruits(0) = "banana"
+
+        fruits(3) = "grape"
+
+
+        Console.WriteLine()
+    End Sub
+
+End Module
