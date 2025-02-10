0edfd9673f9b6c707e214b7bef8dfcbc1be72a7c
Alexis Villagran <alexisvillagran18@hotmail.com>
2025-02-10 08:41:45 -0700

    simple iterate array added

--- ArrayExamples/ArrayExamples.vb
+++ ArrayExamples/ArrayExamples.vb
@@ -9,13 +9,26 @@ Module ArrayExamples
         'Arrays are cool
 
         Dim fruits(5) As String
+        Dim names() As String = {"John", "Paul", "Jeff", "Franklin"}
+        Dim beanCounts = New Integer() {123, 321, 432, 958}
 
         fruits(0) = "banana"
 
         fruits(3) = "grape"
 
 
-        Console.WriteLine()
+        Console.WriteLine(names(3))
+        Console.WriteLine(beanCounts(2))
+
+        'For i = 0 To 10
+        '    Console.WriteLine(names(i))
+        'Next
+
+        For i = LBound(names) To UBound(names)
+            Console.WriteLine(names(i))
+        Next
+
+
     End Sub
 
 End Module
